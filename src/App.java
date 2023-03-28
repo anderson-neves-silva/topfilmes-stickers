f4c21667a47cd70c7a6a90413072c8d108a90206
Anderson Neves <andersonneves_@hotmail.com>
2023-03-28 00:32:27 -0300

    Definindo em comentário o que precisamos fazer

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        // fazer uma conexão HTTP e buscar ps top 250 filmes
+
+        // pegar só os dados que interessam (titulo, poster, classificação)
+
+        // exibir e manipular os dados
     }
 }
