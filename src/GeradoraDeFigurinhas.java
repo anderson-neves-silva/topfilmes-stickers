21acc9faf5c4c310c9c9f201aaa09e7062ab0a33
Anderson Neves <andersonneves_@hotmail.com>
2023-03-29 07:05:14 -0300

    Fazendo a leitura da imagem

---
+++ src/GeradoraDeFigurinhas.java
@@ -0,0 +1,23 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class GeradoraDeFigurinhas {
+  
+  
+  public void cria() throws Exception {
+
+    // leitura da imagem
+    BufferedImage imagemOriginal = ImageIO.read(new File("entrada/filme.jpg"));
+
+    // cria nova imagem em memória com transparência e com tamanho novo
+
+    // copiar a imagem original pra nova imagem (em memória)
+
+    // escrever uma frase na nova imagem
+
+    // escrever a nova imagem em um aquivo
+
+  }
+}
