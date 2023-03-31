1306a1760ab461474835ab3205deb38d9022e343
Anderson Neves <andersonneves_@hotmail.com>
2023-03-31 00:43:07 -0300

    Buscando as imagens de uma url e tratando elas, colocando um texto com uma cor no rodapé da imagem, fazendo a chamada do método cria lá na classe App e fazendo um laço iterando sobre cada objeto

--- src/GeradoraDeFigurinhas.java
+++ src/GeradoraDeFigurinhas.java
@@ -1,16 +1,23 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
 public class GeradoraDeFigurinhas {
   
   
-  public void cria() throws Exception {
+  public void cria(InputStream inputStream, String nomeArquivo) throws Exception {
 
     // leitura da imagem
-    BufferedImage imagemOriginal = ImageIO.read(new File("entrada/filme.jpg"));
-
+    // InputStream inputStream = new FileInputStream(new File("entrada/filme.jpg"));
+    // InputStream inputStream = new URL("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json").openStream();
+    
+    BufferedImage imagemOriginal = ImageIO.read(inputStream);
+ 
     // cria nova imagem em memória com transparência e com tamanho novo
     int largura  = imagemOriginal.getWidth();
     int altura = imagemOriginal.getHeight();
@@ -18,10 +25,19 @@ public class GeradoraDeFigurinhas {
     BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
 
     // copiar a imagem original pra nova imagem (em memória)
+    Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+    graphics.drawImage(imagemOriginal, 0, 0, null);
+
+    // configurar a fonte
+    var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 65);
+    graphics.setFont(fonte);
+    graphics.setColor(Color.RED);
 
     // escrever uma frase na nova imagem
+    graphics.drawString("TOPZERA", 160, novaAltura - 70);
 
     // escrever a nova imagem em um aquivo
-
+    ImageIO.write(novaImagem, "png", new File(nomeArquivo));
   }
+
 }
