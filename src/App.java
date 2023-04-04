7822b9df6ce125a2ec48e750b0163fe8033998ec
Anderson Neves <andersonneves_@hotmail.com>
2023-04-04 00:29:26 -0300

    Criando a classe ClienteHttp

--- src/App.java
+++ src/App.java
@@ -1,34 +1,28 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
 public class App {
-    public static void main(String[] args) throws Exception { 
+    public static void main(String[] args) throws Exception {
 
         // fazer uma conexão HTTP e buscar ps top 250 filmes
-        //String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        // String url =
+        // "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
-        
+
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
+
         // pegar só os dados que interessam (titulo, poster, classificação)
         var parser = new JsonParser();
-        List<Map<String, String>> listaDeConteudos = parser.parse(body);
+        List<Map<String, String>> listaDeConteudos = parser.parse(json);
 
         // exibir e manipular os dados
         System.out.println("======================================================================================");
-        
-        for (Map<String,String> filme : listaDeConteudos) {
-            //String urlImagem = filme.get("image");
+
+        for (Map<String, String> filme : listaDeConteudos) {
+            // String urlImagem = filme.get("image");
             String urlImagem = filme.get("url");
             String titulo = filme.get("title");
 
@@ -40,7 +34,6 @@ public class App {
 
             System.out.println(filme.get("title"));
             System.out.println();
-
-       } 
+        }
     }
 }
